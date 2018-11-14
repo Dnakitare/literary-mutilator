9e7b7db950825397c3eef75c5d41dee4f9423ee7
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-14 15:45:23 -0700

    Added a scramble

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+// More Worf
 function iAmWorf() {
 	document.getElementById("worf").innerHTML = document.getElementById("worf").innerHTML.replace(' I ', '<strong>Worf</strong> ');
 }
-
+// less Worf
 function noWorf() {
 	document.getElementById("worf").innerHTML = document.getElementById("worf").innerHTML.replace('Worf', '');
 }
+
+/*const xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+	console.log(this);
+};
+
+xhr.open('get', 'https://jsonplaceholder.typicode.com/todos/1');
+
+xhr.send();
+*/
+
+// shuffle
+function jibberish() {
+	var worf = document.getElementById("scramble");
+	var string = worf.innerText;
+	var array = string.split(" ");
+
+	for(var i = 0; i < array.length; i++) {
+		var foo = Math.floor(Math.random() * array.length);
+		var bar = array[i];
+		array[i] = array[foo];
+		array[foo] = bar;
+	}
+	array=array.join(" ");
+	console.log(array)
+	document.getElementById("newScramble").innerHTML = array
+};
