8481339f2cebe2e00a672c3c0ac6ee4e5590e40f
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-14 13:54:09 -0700

    Added 'No Worf' functionality

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function iAmWorf() {
 	document.getElementById("worf").innerHTML = document.getElementById("worf").innerHTML.replace(' I ', '<strong>Worf</strong> ');
 }
+
+function noWorf() {
+	document.getElementById("worf").innerHTML = document.getElementById("worf").innerHTML.replace('Worf', '');
+}
