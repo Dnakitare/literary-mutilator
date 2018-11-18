f4deba7e163dc4b5a37e323046ba93ddb5fa9b3f
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-18 14:51:22 -0700

    used Fetch API to insert text into body

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ xhr.open('get', 'https://jsonplaceholder.typicode.com/todos/1');
 xhr.send();
 */
 
+// Grow the t's
+function bigT() {
+	document.getElementById("tees").innerHTML = document.getElementById("tees").innerHTML.replace(/t/gi, '<em>T</em>');
+}
+
+// Shrink the T's
+function lilT() {
+	document.getElementById("tees").innerHTML = document.getElementById("tees").innerHTML.replace(/T/gi, '<b>t</b>');
+}
+
 // shuffle
 function jibberish() {
 	var worf = document.getElementById("scramble");
@@ -32,4 +42,25 @@ function jibberish() {
 	}
 	array=array.join(" ");
 	document.getElementById("newScramble").innerHTML = array
-};
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	// TIER IV
+	// declare global variables
+	const body = document.querySelector('body');
+
+	// fetch API
+	fetch('https://jsonplaceholder.typicode.com/posts/1')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(myJson) {
+			let jsonObj = (myJson['body']);
+			let para = document.createElement('p');
+			para.innerHTML = "<em>Here is some text inserted using fetch API: </em>" + jsonObj;
+			body.appendChild(para);
+
+			console.log(JSON.stringify(para));
+		});
+});
